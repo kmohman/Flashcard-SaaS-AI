d700f082f2a3e3174c3254cf2a3d90ed858cca0d
Khalilullah Mohman <kmohman2@gmail.com>
2024-08-19 17:16:24 -0400

    clerk key update

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,8 @@
 import { clerkMiddleware } from "@clerk/nextjs/server";
 
-export default clerkMiddleware();
+export default clerkMiddleware({
+  publishableKey: process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
+});
 
 export const config = {
   matcher: [
